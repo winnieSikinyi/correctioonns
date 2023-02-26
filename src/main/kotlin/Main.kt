ab96236efcb1da8110a1e38f36b7c082d9fbc98e
winnieSikinyi <winniesikinyi@gmail.com>
2023-02-26 13:13:31 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+createString()
+  Statement("Winnie",30)
+    println(word ("kingsprng"))
+    text("winsy")
+}
+//
+    fun createString (){
+    var name = "akirachix"
+        var out = "${name[0]}${name[2]}${name[3]}"
+println(out) }
+
+//
+fun Statement (name:String,age:Int){
+    var sentence = "Hi,my name is $name and i am $age years old"
+    println(sentence)
+}
+//
+fun word(length:String):String{
+    return ("The length of the length string is" +length.length)
+
+}
+//
+fun text (mane:String){
+    var beatiful = "winsy"
+    if (mane  == "winsy"){
+        println("thats me")
+    }
+        else {
+        println("thats not me")
+    }
+
+
+}
+
